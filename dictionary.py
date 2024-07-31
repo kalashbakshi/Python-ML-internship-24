cfbd75476260be2db1216e685426f9747c143c06
kalashbakshi <120623858+kalashbakshi@users.noreply.github.com>
2024-07-31 22:47:40 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+#dictionary
+d={1:"Abcd","xyz":123,2:[1,2,3,4]}
+print(d.values())
+print(d.keys())
---
+++ if else.py
@@ -0,0 +1,10 @@
+# if else
+n=int(input())
+if n<0:
+  if n>(-24):
+     if n%2==0:
+         print("good")
+     print("not Successful")
+  print("not valid")
+else:
+    print("bad")
